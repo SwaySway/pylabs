ce4523f44fee04ac68ca4d5081f016b5916d0db2
Josue Ruiz <josueruiz4747@gmail.com>
2016-10-18 22:46:40 -0700

    Fixed errors in lab4

--- lab4.py
+++ lab4.py
@@ -1,12 +1,37 @@
 import math
+
 """
 Josue Ruiz
 CS 299
-
-
-First Problem of Lab 4
+Lab 4
 """
-# Part A
+
+
+def median(data_lst):
+    data_lst = sorted(data_lst)
+    len_size = len(data_lst)
+    position = (len_size - 1) // 2
+
+    if len_size % 2:
+        return data_lst[position]
+    else:
+        return (data_lst[position] + data_lst[position + 1])/2.0
+
+
+def index(d_list, pos):
+    list_size = len(d_list)
+    if pos not in d_list:
+        return -1
+    for i in range(list_size):
+        if pos is d_list[i]:
+            return i
+
+
+def reverse(d_lst):
+    return d_lst[::-1]
+
+
+# First Part - Part A
 lst = ["Hello", 7, 7, 9, 'a', 'cat', False]
 print("Created list:\n", lst)
 
@@ -35,3 +60,23 @@ print("Removed the first 7 from the list\n", lst)
 lst.pop(lst.index('dog'))
 print("List with dog removed by finding the index and then removing with pop\n", lst)
 
+
+# Second Part of assignment
+# Part A
+lst = reverse(lst)
+print("Reversed List from Part 1\n", lst)
+
+# Part B
+rev_lst = index(lst, 'armor')
+print("Trying to find word that doesn't exist such as \"armor\" \n", rev_lst)
+rev_lst2 = index(lst, 'cat')
+print("Trying to find word that exists such as \"cat\" \n", rev_lst2)
+
+
+# Part C
+aList = [24, 5, 8, 2, 9, 15, 10]
+med_aList = median(aList)
+print("Current List ", aList, "\nImplemented Median:\n", med_aList)
+bList = [24, 5, 8, 2, 9, 15, 10, 54]
+med_bList = median(bList)
+print("Current List ", bList, "\nImplemented Median:\n", med_bList)
